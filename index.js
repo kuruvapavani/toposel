31c6c2b1a969c82c2b09cf877a6958e4ae037d42
kuruvapavani <pavanikuruva2109@gmail.com>
2025-02-13 22:12:30 +0530

    setup server

---
+++ index.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.listen(port, (error) =>{
+    if(!error)
+        console.log("Server is Successfully Running, and App is listening on port "+ port)
+    else 
+        console.log("Error occurred, server can't start", error);
+    }
+);
